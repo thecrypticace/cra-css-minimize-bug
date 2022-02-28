4457d38a81208e3ac230f3470ab93d9db55ce4c1
Jordan Pittman <jordan@cryptica.me>
2022-02-28 11:18:17 -0500

    Reduce test case

--- src/App.js
+++ src/App.js
@@ -1,28 +1,5 @@
-import logo from './logo.svg';
-
-// Styling
 import './styles/custom.css';
 
-import BuildFail from './components/buildFail/BuildFail';
-
-function App() {
-  return (
-    <div className="mx-auto text-white">
-      <header className="bg-gray-800 min-h-[100vh] flex flex-col items-center justify-center text-xl text-white">
-        <div>
-          <p>This is a vanilla CRA5 app configured with Tailwind 3.</p>
-          <p>The project runs successfully (<code>npm run start</code>)&hellip;</p>
-          <p>&hellip; but it fails to build (<code>npm run build</code>):</p>
-        </div>
-        <BuildFail />
-
-        <div className='mt-4'>
-          <p>To fix the build problem, delete <code>bg-white</code> from line 2 of <code>./styles/custom.css</code> and re-run the build command.</p>
-        </div>
-
-      </header>
-    </div>
-  );
+export default function App() {
+  return <div></div>;
 }
-
-export default App;
